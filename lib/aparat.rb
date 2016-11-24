ec1cfe033c2f65084690e1fdad295b854d1fe1ae
Muhammadreza Haghiri <haghiri75@gmail.com>
2016-11-25 00:21:22 +0330

    videos method added

--- lib/aparat.rb
+++ lib/aparat.rb
@@ -1,4 +1,13 @@
+require 'json'
+require 'net/http'
+require 'uri'
 # This is a simple client for Iranian video-sharing website, aparat.com 
 
 class Aparat
+ #This method returns information from a video
+ def self.video(hash)
+  uri = URI.parse("http://aparat.com/etc/api/video/videohash/#{hash}")
+  response = Net::HTTP.get_response(uri)
+  return JSON.parse(response.body)
+ end
 end
